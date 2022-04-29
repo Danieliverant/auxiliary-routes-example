e1d0ad34e99a4ead8dd1ba72c84bf34931e775be
Daniel private <liverant95@gmail.com>
2022-04-29 18:29:05 +0300

    fix(global): tidy code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AsideComponent } from './components/aside/aside.component';
-import { ContextMenuComponent } from './components/context-menu/context-menu.component';
-import { PlaylistComponent } from './modules/playlist/playlist.component';
-import {
-  globalContextMenuItems,
-  playlistContextMenuItems,
-} from './components/context-menu/context-menu-item/context-menu-item.model';
-import { HomeComponent } from './modules/home/home.component';
 
 const routes: Routes = [
   {
